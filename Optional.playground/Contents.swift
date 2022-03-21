61490f08155e25c39b17ff1895e89d6e4d165856
songgyeol <songgyeol@kakao.com>
2022-03-21 17:25:52 +0900

    eggTimer_Optional

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+//var player1Username: String = nil
+var player1Username: String? = nil
+
+player1Username = "jackbaueriawesome"
+
+//var unwrappedP1Username = player1Username!
+player1Username = nil
+
+if player1Username != nil {
+    print(player1Username!)
+}
