ab8b07c439c47516e9999465349d21c19f296a6a
songgyeol <songgyeol@kakao.com>
2022-03-24 20:43:06 +0900

    Struct_example

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,29 @@
 
 
 
-func greeting() {
-    print("hello")
-
+struct Town {
+        // Properties
+    let name: String
+    var citizens = [String]()
+    var resources = [String: Int]()
+    
+    init(name: String, citizens: [String], resources: [String: Int]) {
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+    
+    /*    매개변수
+    init(<#parameters#>) {
+        <#statements#>
+    }*/
+    
+        // Method
+    func fortify() {
+            print("Defences increased!")
+    }
 }
 
-greeting()
+var anotherTown = Town(name: "Nameless Island", citizens: ["Tom Hanks"], resources: ["Coconuts": 100])
+anotherTown.citizens.append("Wilson")
+print(anotherTown.citizens)
